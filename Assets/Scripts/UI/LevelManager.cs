06f2f16c94d5c19bf7d14fddbf2b5a1439ebec01
GrigoryEbl <123863131+GrigoryEbl@users.noreply.github.com>
2023-11-21 22:21:43 +0400

    WorkSDK7

--- Assets/Scripts/UI/LevelManager.cs
+++ Assets/Scripts/UI/LevelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,34 +10,36 @@ public class LevelManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _buttons;
 
-    private void OnEnable() => YandexGame.GetDataEvent += ChangeOpenLevels;
+    private void OnEnable() => YandexGame.GetDataEvent += LoadGame;
 
-    private void OnDisable() => YandexGame.GetDataEvent -= ChangeOpenLevels;
+    private void OnDisable() => YandexGame.GetDataEvent -= LoadGame;
 
-    private void Awake()
+    private void Start()
     {
-        if (YandexGame.SDKEnabled == true)
+        if(YandexGame.SDKEnabled)
         {
-            ChangeOpenLevels();
-            SetDefaultOpenLevels();
+            LoadGame();
         }
     }
 
     private void SetDefaultOpenLevels()
     {
+        for (int i = 0; i <= YandexGame.savesData.openLevels.Length - 1; i++)
+        {
+            YandexGame.savesData.openLevels[i] = false;
+        }
+
         YandexGame.savesData.openLevels[0] = true;
         YandexGame.savesData.openLevels[1] = true;
     }
 
-    public void ChangeOpenLevels()
+    public void ChangeOpenedLevels()
     {
-        SetDefaultOpenLevels();
-
         for (int i = 0; i <= YandexGame.savesData.openLevels.Length - 1; i++)
         {
-            if (YandexGame.savesData.openLevels[i] == false)
+            if (YandexGame.savesData.openLevels[i] == true)
             {
-                _buttons[i].SetActive(false);
+                _buttons[i].SetActive(true);
             }
         }
     }
@@ -45,11 +48,18 @@ public class LevelManager : MonoBehaviour
     {
         YandexGame.ResetSaveProgress();
         SetDefaultOpenLevels();
+        ChangeOpenedLevels();
         YandexGame.SaveProgress();
     }
 
-    public void LoadLevel(int index)
+    private void LoadGame()
     {
-        SceneManager.LoadScene(index);
+        ChangeOpenedLevels();
+
+        if (YandexGame.savesData.isFirstSession)
+        {
+            ResetProgress();
+            ChangeOpenedLevels();
+        }
     }
 }
